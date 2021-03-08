367a4bc5b7fdfe95c667c0bb2e924a3048bcb3be
mikkelswartz <62695198+mikkelswartz@users.noreply.github.com>
2021-03-08 13:06:02 +0100

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -10,12 +10,3 @@ def install_func(package):
     else:
         pip._internal.main(['install', package])
 
-import subprocess
-import sys
-
-try:
-    import pdftotext
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", 'install', 'pdftotext'])
-finally:
-    import pdftotext
