abd6e8b2c7f8ef642919711b3db4ee6936580237
mikkelswartz <62695198+mikkelswartz@users.noreply.github.com>
2021-03-08 12:48:37 +0100

    Add files via upload

--- Installer.py
+++ Installer.py
@@ -2,9 +2,20 @@ import streamlit as st
 
 import pip
 
+
 def install_func(package):
     if hasattr(pip, 'main'):
         pip.main(['install', package])
+
     else:
         pip._internal.main(['install', package])
-    
+
+import subprocess
+import sys
+
+try:
+    import pdftotext
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", 'install', 'pdftotext'])
+finally:
+    import pdftotext
